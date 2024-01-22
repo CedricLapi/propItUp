cb812e5cecc8fe677413e9d5390407772b763348
Cedric Lapi <lapicedric@gmail.com>
2024-01-22 15:40:18 +0100

    hairColor updated!

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -9,10 +9,12 @@ function App() {
 
   return (
     <>
-      <PersonCard firstName={"Ilke"} lastName={"Lea"} age={35} haircolor={"brown"} /><hr />
-      <PersonCard firstName={"Bandile"} lastName={"Thabo"} age={25} haircolor={"black"} /><hr />
-      <PersonCard firstName={"Felix"} lastName={"Okonta"} age={58} haircolor={"black"} /><hr />
-      <PersonCard firstName={"Joannes"} lastName={"Bester"} age={50} haircolor={"blond"} /><hr />
+      <PersonCard firstName={"Ilke"} lastName={"Lea"} age={35} hairColor={"brown"} /><hr />
+      <PersonCard firstName={"Bandile"} lastName={"Thabo"} age={25} hairColor={"black"} /><hr />
+      <PersonCard firstName={"Felix"} lastName={"Okonta"} age={58} hairColor={"black"} /><hr />
+      <PersonCard firstName={"Joannes"} lastName={"Bester"} age={50} hairColor={"blond"} /><hr />
+
+      <button onClick={ () => alert("This button has been clicked!")}> Click Here! </button>
     </>
   )
 }
