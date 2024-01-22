2418f43a60b0eddabc82af732c3530c7a4d45d9c
Cedric Lapi <lapicedric@gmail.com>
2024-01-22 15:08:51 +0100

    completed!

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -2,32 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PersonCard from './components/PersonCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PersonCard firstName={"Ilke"} lastName={"Lea"} age={35} haircolor={"brown"} /><hr />
+      <PersonCard firstName={"Bandile"} lastName={"Thabo"} age={25} haircolor={"black"} /><hr />
+      <PersonCard firstName={"Felix"} lastName={"Okonta"} age={58} haircolor={"black"} /><hr />
+      <PersonCard firstName={"Joannes"} lastName={"Bester"} age={50} haircolor={"blond"} /><hr />
     </>
   )
 }
